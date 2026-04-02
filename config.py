6166131618b22f4d287fe01d1692ecf3a086bb6e
yxa174 <k851km@gmail.com>
2026-04-02 14:02:42 +0700

    добавил отступ

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+
 BYBIT_API_KEY    = os.getenv("BYBIT_API_KEY")
 BYBIT_API_SECRET = os.getenv("BYBIT_API_SECRET")
 TELEGRAM_TOKEN   = os.getenv("TELEGRAM_TOKEN")
