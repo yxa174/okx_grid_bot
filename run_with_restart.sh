07905b9d14a25b4fd4172698925f12ff9756dd8f
yxa174 <k851km@gmail.com>
2026-04-06 17:19:24 +0700

    add function anti down bot

---
+++ run_with_restart.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Автоматический перезапуск бота при падении
+MAX_RESTARTS=10
+RESTART_COUNT=0
+
+while true; do
+    echo "[$(date)] Запуск бота (попытка $((RESTART_COUNT+1)))"
+    cd /Users/a123/Documents/code/okx_bot_test
+    ./venv/bin/python okx_grid_bot.py 2>&1 | tee -a bot.log
+    
+    EXIT_CODE=$?
+    RESTART_COUNT=$((RESTART_COUNT+1))
+    
+    if [ $EXIT_CODE -eq 0 ]; then
+        echo "[$(date)] Бот остановлен корректно"
+        break
+    fi
+    
+    if [ $RESTART_COUNT -ge $MAX_RESTARTS ]; then
+        echo "[$(date)] Достигнут лимит перезапусков ($MAX_RESTARTS). Остановка."
+        break
+    fi
+    
+    echo "[$(date)] Бот упал (код: $EXIT_CODE). Перезапуск через 30 сек..."
+    sleep 30
+done
